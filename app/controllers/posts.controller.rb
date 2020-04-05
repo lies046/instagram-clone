89182a8d314682181d12d06fcc77dca8c77c2c12
lies046 <skytree.skysky@gmail.com>
2020-04-05 20:16:01 +0900

    Add CarrierWave and AccountsController

---
+++ app/controllers/posts.controller.rb
@@ -0,0 +1,17 @@
+class Postscontroller < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+  
+  def show
+  end
+
+  private
+  
+  def post_params
+  params.require(:post).permit(:image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :accounts
+
+  get "/dashboard" => "accounts#index"
+
+  resouces :posts only:[:new, :create, :show]
   root to: "public#homepage"
 end
