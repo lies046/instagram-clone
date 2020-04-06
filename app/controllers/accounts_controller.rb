a73cd75714a36660273b25143f0da695b80d46a3
lies046 <skytree.skysky@gmail.com>
2020-04-06 20:37:02 +0900

    Modified AccountsProfile

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,11 +3,19 @@ class AccountsController < ApplicationController
   before_action :set_account, only: [:profile ]
   def index
     @posts = Post.active
+    @follower_suggestions = Account.where.not(id: current_account.id)
   end
 
   def profile
     @posts = @account.posts.active
   end
+
+  def follow_account
+    follower_id = params[:follow_id]
+    Follower.create(follower: current_account.id, following: follower_id)
+  end
+
+  private
 
   def set_account
     @account = Account.find_by_username(params[:username])
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_072911) do
+ActiveRecord::Schema.define(version: 2020_04_06_113043) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2020_04_06_072911) do
     t.string "website"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "followers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "follower_id"
+    t.bigint "following_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id"], name: "index_followers_on_follower_id"
+    t.index ["following_id"], name: "index_followers_on_following_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
