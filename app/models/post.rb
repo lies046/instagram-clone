c221736abade47fde8097d5cff31310b83aefeea
lies046 <skytree.skysky@gmail.com>
2020-04-06 12:33:01 +0900

    Modified AccountsIndexView

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,11 @@ class Post < ApplicationRecord
   before_create :set_active
   scope :active, -> {where active: true}
 
+  def total_likes
+    0
+  end
+  
+  private
   def set_active
     self.active = true
   end
