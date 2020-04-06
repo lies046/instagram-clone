cf7d8c7da7d0b00623ecb2e19fc6038c30634fa5
lies046 <skytree.skysky@gmail.com>
2020-04-06 12:10:56 +0900

    Modified AccountsView

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_013542) do
+ActiveRecord::Schema.define(version: 2020_04_06_030812) do
 
   create_table "accounts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_013542) do
     t.bigint "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["account_id"], name: "index_posts_on_account_id"
   end
 
