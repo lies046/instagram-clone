1370df4fa227f0f7581301a888f3a5afabac6c38
lies046 <skytree.skysky@gmail.com>
2020-04-07 21:01:39 +0900

    Modified AccountsProfile

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,12 +2,16 @@ class AccountsController < ApplicationController
   before_action :authenticate_account!
   before_action :set_account, only: [:profile ]
   def index
-    @posts = Post.active
-    
+
+    followers_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
+    followers_ids << current_account.id 
+    @posts = Post.includes(:account).where(account_id: followers_ids)
+    @comment = Comment.new
+
     following_ids = Follower.where(follower_id: current_account.id).map(&:following_id)
     following_ids << current_account.id
     @follower_suggestions = Account.where.not(id: following_ids)
-    @comment = Comment.new
+    
   end
 
   def profile
--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,7 +2,7 @@ module AccountsHelper
 
   def profile_picture account, width = 100
     image_path = account.image.present? ? account.image.url : "default.png"
-    image_tag(image_path,width: width, class: "img-circle")
+    image_tag(image_path,width: width, class: "img-circle m-r-10")
   end
 
   def can_edit_profile? profile_id
